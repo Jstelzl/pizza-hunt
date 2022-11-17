f11d3419b44d9b56a0cb755aa6f0652cf040a9b8
[josiah stelzl] <josiahstelzl@gmail.com>
2022-11-17 09:42:41 -0500

    added validator to pizza controller

--- models/Pizza.js
+++ models/Pizza.js
@@ -5,9 +5,13 @@ const PizzaSchema = new Schema(
     {
         pizzaName: {
             type: String,
+            required: true,
+            trim: true
         },
         createBy: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
         },
         createAt: {
             type: Date,
@@ -16,6 +20,8 @@ const PizzaSchema = new Schema(
         },
         size: {
             type: String,
+            required: true,
+            enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
             default: 'Large'
         },
         toppings: [],
